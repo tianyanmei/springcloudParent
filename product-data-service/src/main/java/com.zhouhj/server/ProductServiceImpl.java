f42c9b06c4f124b5a3baddfd156dc495bcfa2296
tianyanmei <a168209@sina.cn>
2019-07-26 16:57:57 +0800

    第一版集成EurekaServer服务，注册数据微服务（数据服务中心）

---
+++ product-data-service/src/main/java/com.zhouhj/server/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package com.zhouhj.server;
+
+import com.zhouhj.pojo.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl {
+
+    @Value("${server.port}")
+    String port;
+
+    public List<Product> productList(){
+        List<Product> ps = new ArrayList<>();
+        ps.add(new Product(1,"product a from port:"+port, 50));
+        ps.add(new Product(2,"product b from port:"+port, 150));
+        ps.add(new Product(3,"product c from port:"+port, 250));
+        return ps;
+
+    }
+
+}
