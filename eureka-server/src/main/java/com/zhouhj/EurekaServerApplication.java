40db638f7f52414c8c8374abf1643ba114959611
tianyanmei <a168209@sina.cn>
2019-07-26 16:10:02 +0800

    第一版集成EurekaServer服务，springcloud 服务注册中心

---
+++ eureka-server/src/main/java/com/zhouhj/EurekaServerApplication.java
@@ -0,0 +1,26 @@
+package com.zhouhj;
+
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        // SpringApplication.run(EurekaserverApplication.class, args);
+        int port=8762;
+        if(!NetUtil.isUsableLocalPort(port)){
+            System.out.printf("端口%d被占用了，无法启动%n", port );
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(EurekaServerApplication.class).
+                properties("server.port="+port).run(args);
+
+    }
+
+
+}
